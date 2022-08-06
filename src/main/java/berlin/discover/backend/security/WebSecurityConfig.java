c8f2906acfe9940d1d045762f1853137ba3b7e0f
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2022-08-06 10:59:36 +0200

    Injects passwor encoder in web security configuration file, and injects a dao authentication provider.

--- src/main/java/berlin/discover/backend/security/WebSecurityConfig.java
+++ src/main/java/berlin/discover/backend/security/WebSecurityConfig.java
@@ -4,10 +4,11 @@ import berlin.discover.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,26 +17,40 @@ public class WebSecurityConfig {
 
     private final UserService userService;
 
+    private final PasswordEncoder passwordEncoder;
+    
     @Bean
     public UserDetailsService userDetailsService() {
         return userService;
     }
 
-    /*
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    */
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return null;
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin();
+
+        /*
+                .antMatchers("/login").permitAll()
+                .antMatchers("/api/admin").hasAuthority("Admin")
+                .antMatchers("/api/users").hasAuthority("Admin")
+                .anyRequest().authenticated();
+        */
+        return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return null;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
     }
 
 }
